f42c39b8662f5d6431565efb8c5860f2f26b5cc2
Shakeel Nawaz <75359115+Shakeel-Nawaz@users.noreply.github.com>
2021-03-07 00:28:20 +0530

    Update settings.py

--- songpodcastaudiobookapi/settings.py
+++ songpodcastaudiobookapi/settings.py
@@ -112,3 +112,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
+# }
